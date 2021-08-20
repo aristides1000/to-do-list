7dad8bf6142a2d34d8ade32bad5c6a91447de2e9
Aristides Jose Molina Perez <aristides1000@gmail.com>
2021-08-20 15:49:30 -0400

    Implement a function for deleting a task (remove an element from the array).

--- src/add-and-remove.js
+++ src/add-and-remove.js
@@ -1,7 +1,14 @@
+import { saveDataLocalStorage, loadDataLocalStorage } from './index.js';
+
 function addTask(activities, description) {
   let newActivity = { description: description, index: activities.length, completed: false };
   activities.push(newActivity);
   return activities;
 }
 
-export { addTask };
+function deleteTask(activities, index) {
+  activities.splice(index, 1);
+  return activities;
+}
+
+export { addTask, deleteTask };
--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@
 /* eslint-disable import/no-cycle */
 /* eslint-disable no-plusplus */
 /* eslint-disable no-unused-vars */
-import _ from 'lodash';
+import _, { delay, slice } from 'lodash';
 import './style.css';
 import { checked, textDecorationChecked, changeState } from './checked.js';
-import { addTask } from './add-and-remove.js';
+import { addTask, deleteTask } from './add-and-remove.js';
 
 const ul = document.querySelector('.task-list');
 
@@ -27,12 +27,15 @@ function displayTasks() {
     const li = document.createElement('li');
     const checkIn = checked(activities, i);
 
-    li.innerHTML = `<li class="border p-3 d-flex justify-content-between" id="${activities[i].index}">
+    li.innerHTML = `<li class="border p-3 d-flex justify-content-between" id="${i}">
     <div class="d-inline">
       <input class="form-check-input" type="checkbox" value="" id="checkTask${i}" ${checkIn}>
       <input type="text" class="border-0 onfocus-border-none ps-2 w-75" value="${activities[i].description}" id="taskInput${i}">
     </div>
-    <a href="#" class="color-text-black"><ion-icon name="ellipsis-vertical"></ion-icon></a>
+    <div>
+      <a href="#" class="color-text-black" id="verticalPoints${i}"><ion-icon name="ellipsis-vertical"></ion-icon></a>
+      <a href="#" class="color-text-black d-none" id="delete${i}"><ion-icon name="trash"></ion-icon></a>
+    </div>
   </li>`;
 
     ul.appendChild(li);
@@ -51,6 +54,34 @@ function displayTasks() {
     const checkTask = document.getElementById(`checkTask${i}`);
 
     changeState(activities, i, checkTask);
+
+    const verticalPoints = document.getElementById(`verticalPoints${i}`);
+    const trashCan = document.getElementById(`delete${i}`);
+
+    const container = document.getElementById(i);
+    
+    taskInput.addEventListener('focus', () => {
+      verticalPoints.classList.add('d-none');
+      trashCan.classList.remove('d-none');
+      container.classList.add('bg-warning');
+      taskInput.classList.add('border-0', 'onfocus-border-none', 'bg-warning');
+    });
+
+    taskInput.addEventListener('focusout', () => {
+      setTimeout(() => {
+      verticalPoints.classList.remove('d-none');
+      trashCan.classList.add('d-none');
+      container.classList.remove('bg-warning');
+      taskInput.classList.remove('bg-warning');
+      }, 200);
+    });
+
+    trashCan.addEventListener('click', () => {
+      let data = loadDataLocalStorage();
+      data = deleteTask(activities, i);
+      saveDataLocalStorage(data);
+      displayTasks();
+    });
   }
 }
 
@@ -66,4 +97,4 @@ addNewDescription.addEventListener('click', () => {
 
 displayTasks();
 
-export { displayTasks, saveDataLocalStorage };
+export { displayTasks, saveDataLocalStorage, loadDataLocalStorage };
