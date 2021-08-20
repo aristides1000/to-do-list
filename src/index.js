debcdc83f274cec742e551677b21cabde49c8b41
Aristides Jose Molina Perez <aristides1000@gmail.com>
2021-08-20 16:03:06 -0400

    Deleting a task should update all remaining items' indexes, so they represent the current list order and are unique.

--- src/index.js
+++ src/index.js
@@ -29,7 +29,7 @@ function displayTasks() {
 
     li.innerHTML = `<li class="border p-3 d-flex justify-content-between" id="${i}">
     <div class="d-inline">
-      <input class="form-check-input" type="checkbox" value="" id="checkTask${i}" ${checkIn}> 
+      <input class="form-check-input" type="checkbox" value="" id="checkTask${i}" ${checkIn}>
       <input type="text" class="border-0 onfocus-border-none ps-2 w-75" value="${activities[i].description}" id="taskInput${i}">
     </div>
     <div>
